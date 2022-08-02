8c928afd5de88882d88e1b01c56334d369b5adb6
AlifSakib <alifsakib@gmail.com>
2022-08-02 17:32:50 +0600

    Arrays

--- 22-JavaScript-Array-Methods.js
+++ 22-JavaScript-Array-Methods.js
@@ -1,1 +1,23 @@
 // 22-JavaScript-Array-Methods.js
+
+// The JavaScript method toString() converts an array to a string of (comma separated) array values.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(fruits.toString())
+
+// The join() method also joins all array elements into a string. but we can define with what we can to join
+
+console.log(fruits.join(" "))
+
+// pop returns the value and we can store the valuse too in a new variable
+
+
+// !Shifting Element
+
+// shift() removes the from the 1st element of an Array
+// unshift() take a value hat we want to add beginig of an array
+// delet keyword use ffor delet element from array
+// deleting an element from array leave the removed element space as empty
+
+// !Splice
