f6dd964bdb98e9b5e2be872d3c5478690294e51c
AlifSakib <alifsakib@gmail.com>
2022-08-02 21:47:04 +0600

    Loop

---
+++ 23-JavaScript-Array-Sort.js
@@ -0,0 +1,27 @@
+// 23-JavaScript-Array-Sort.js
+
+// ?The sort() method sorts an array alphabetically:
+// ?The reverse() method reverses the elements in an array.
+
+// !When we want to sort a array which contain numbers it will not sort the numbers corectly because he calculate number as string so for that we will use a function inside they sort() ->
+
+// this is for assending format
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+
+// array descending:
+const point = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return b - a});
+
+
+
+
+
+for (i = 30 ; i <= 86 ; i++){
+    
+    console.log(i)
+    if(i == 44){
+        break;
+    }
+    
+}
---
+++ practice-loop.js
@@ -0,0 +1,2 @@
+// *Write a JS code to delete all occurrence of element in given array
+
