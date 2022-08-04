2a0cd6ad9456de378290c89cb136f6b3f009dc0c
AlifSakib <alifsakib@gmail.com>
2022-08-04 20:20:06 +0600

    Math

--- 26-JavaScript-Dates.js
+++ 26-JavaScript-Dates.js
@@ -1,1 +1,17 @@
 // 26-JavaScript-Dates.js
+
+d = new Date();
+
+console.log(d)
+
+// The toUTCString() method converts a date to a UTC string (a date display standard).
+
+console.log(d.toUTCString())
+
+// *Output : Thu, 04 Aug 2022 13:39:07 GMT
+
+// The toDateString() method converts a date to a more readable format:
+
+console.log(d.toDateString())
+
+// *Output : Thu Aug 04 2022
