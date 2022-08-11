c28b33cbd476a8fbe34fbf63b2a9a5d0e5432577
AlifSakib <alifsakib@gmail.com>
2022-08-12 02:20:05 +0600

    03-JavaScript-Statement.js

--- 02-JavaScript-Output.js
+++ 02-JavaScript-Output.js
@@ -1,1 +1,9 @@
-// TODO: 02-JavaScript-Output.js;
+// TODO: 02-JavaScript-Output.js
+
+// There are many ways how we can show Output for JavaScript
+
+// * 1. window.alert()
+// * 2. document.write()
+// * 3. console.log()
+// * 4. window.print()
+// * 5. innerHTML
