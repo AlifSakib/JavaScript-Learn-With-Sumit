de5832ec0e10d6ff1168b9faf4e3f72b0f4c2474
AlifSakib <alifsakib@gmail.com>
2022-08-11 20:26:18 +0600

    Introduction to JavaScript

---
+++ 01-Introduction-To-Javacript.js
@@ -0,0 +1,5 @@
+// TODO: 01-Introduction-To-Javacript.js
+
+// Java Script Invented in 1995
+// ES = Ecma Script // Javascript Official Name is EcmaScript
+// ECMA -> European Computer Manufacturer's Association
