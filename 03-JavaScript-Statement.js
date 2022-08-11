9c4707229d04f6259a0e0e7fd046aa263af5a6ca
AlifSakib <alifsakib@gmail.com>
2022-08-12 03:09:33 +0600

    03-JavaScript-Statement.js

--- 03-JavaScript-Statement.js
+++ 03-JavaScript-Statement.js
@@ -1,1 +1,19 @@
-// TODO: 03-JavaScript-Statement.js;
+// TODO: 03-JavaScript-Statement.js
+
+//A computer program is a list of "instructions" to be "executed" by a computer.
+
+//In a programming language, these programming instructions are called statements.
+
+//A JavaScript program is a list of programming statements.
+
+// ? JavaScript Statements are made with those things
+
+// * 1 Values
+// * 2 Operators
+// * 3 Expression
+// * 4 Keywords
+// * 5 Comments
+
+//? JavaScript Keywords
+
+var , let , const , if , switch , for , function , return , try
