bb17ff69cdbcfdb66f3b45a10a12b3dcac51291b
AlifSakib <alifsakib@gmail.com>
2022-08-12 17:24:12 +0600

    09-JavaScript-Operators.js

---
+++ 09-JavaScript-Operators.js
@@ -0,0 +1,16 @@
+// TODO: 09-JavaScript-Operators.js
+
+let x = 5 + 5;
+
+let y = "5" + 5; // jokhon string and number jog korte jabo tokhon string priority pabe beshhi . and add kore string return korbe.
+
+// !JavaScript Type Operator
+
+// * typeof -> Return the typeof a variable
+// * instanceof -> Returns true if an object is an instance of an object type.
+
+// ? Is  Typeof is a operator ? Yes
+
+// ! Operator vs Operand
+
+//-> Operator er left and right a jara thake tara hochhe operand.
