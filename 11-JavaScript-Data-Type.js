6aa690587ab7213b68fba21f830200e77d53fbea
AlifSakib <alifsakib@gmail.com>
2022-08-12 20:19:07 +0600

    11-JavaScript-Data-Type.js

---
+++ 11-JavaScript-Data-Type.js
@@ -0,0 +1,36 @@
+// TODO: 11-JavaScript-Data-Type.js
+
+// ?JavaScript is a high level language , thats mean JavaScript is far away from machine . Its run on the browser . JavaScript types are dynamic.
+
+// !IMORTANT: JavaScript evalutes expression from left to right. Different sequences can produce different result :
+
+console.log(6 + 4 + "AUDI"); // * OutPut : 10AUDI
+console.log("AUDI" + 6 + 4); // * OutPut : AUDI64
+
+// jehetu left to right read korbe and she always duita kore nobe so output gula erokom ashbe.
+
+//JavaScript is also a Dinamic Language .
+// ! JavaScript String  [Data Type]
+// ! JavaScript Numbers [Data Type]
+
+// 1. Numbers can be a Single Number or It can be Floating Number.
+
+// 2. Extra large or small number can be written like below Example :
+
+let y = 123e5;
+let r = 123e-5;
+
+console.log(y, r);
+
+// ! JavaScript Booleans [Data Type]
+
+// ! JavaScript Array    [Data Type]
+//      Array written with [ ] bracket
+
+// ! JavaScript Objects  [Data Type]
+//      Object written with { } bracket
+
+// ! JavaScript Undefined [Data Type] [Its also a value]
+//      Its a Keyword , Mane defined kora hoy nai.
+
+// ! JavaScript Empty Value
