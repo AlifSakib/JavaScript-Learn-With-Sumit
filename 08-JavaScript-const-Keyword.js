4f2caa0cd38996d017c721a40602aa251bad3737
AlifSakib <alifsakib@gmail.com>
2022-08-12 11:02:07 +0600

    08-JavaScript-const-Keyword.js

---
+++ 08-JavaScript-const-Keyword.js
@@ -0,0 +1,25 @@
+// TODO: 08-JavaScript-const-Keyword.js
+
+// TODO: !When to use const ?
+
+// * 1 . A new Array
+// * 2 . A new Object
+// * 3 . A new Function
+// * 4 . A new RegExp
+
+//? Constant Objects & Arrays
+
+// const doesnot reffer to a canstant value . It defines a constant reference to a value
+// When we declare const eta memory te value take const define kore na , value jar moddhe store oitare define kore .
+
+/* Thats why we can not : 
+
+   1 reassign a constant Value
+   2 reassign a constant Array
+   3 reassign a constant Object 
+
+   But We Can :
+   
+   Change a constant Array [] Or Object {} */
+
+// ?let er moto const eo hoisting e upore niye jay but ekta deadzone make hoy
