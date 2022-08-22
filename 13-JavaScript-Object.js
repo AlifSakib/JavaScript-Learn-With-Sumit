3b2a7798f92e12ed632b1acb8e67594d7a1c3d85
AlifSakib <alifsakib@gmail.com>
2022-08-23 05:53:50 +0600

    13-JavaScript-Object.js

---
+++ 13-JavaScript-Object.js
@@ -0,0 +1,36 @@
+//TODO: 13-JavaScript-Object.js
+
+let car = {
+  name: "Fiat",
+  model: 500,
+  weight: "850kg",
+  color: "white",
+
+  start: function () {
+    this.drive();
+    console.log("Car Has Started");
+  },
+  drive: function () {
+    console.log("Car Is Driving");
+  },
+};
+
+car.start();
+console.log(car.name);
+console.log(car["model"]);
+
+let person = {
+  firstName: "Jhon",
+  lastName: "Doe",
+  id: 5666,
+  fullname: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+let firstName = person.fullname();
+console.log(firstName);
+
+let x = new String("Rahim");
+
+console.log(x);
