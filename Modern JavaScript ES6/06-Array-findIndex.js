c9a923ba83333271bf2687d873b868d6bb58fff8
AlifSakib <alifsakib@gmail.com>
2022-09-02 08:27:29 +0600

    06-Array-findIndex.js

--- Modern JavaScript ES6/06-Array-findIndex.js
+++ Modern JavaScript ES6/06-Array-findIndex.js
@@ -11,3 +11,16 @@ const result = numbers.findIndex((currentValue, currentIndex, arr) => {
 console.log(result);
 
 // etar khetreo this pass kora jay find er moto
+
+/* 
+! Definition and Usage
+
+The findIndex() method executes a function for each array element.
+
+The findIndex() method returns the index (position) of the first element that passes a test.
+
+The findIndex() method returns -1 if no match is found.
+
+The findIndex() method does not execute the function for empty array elements.
+
+The findIndex() method does not change the original array. */
