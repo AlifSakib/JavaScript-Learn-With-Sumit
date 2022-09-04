eea4e4ebfd241ef28bf837f00919259c572c4258
AlifSakib <alifsakib@gmail.com>
2022-09-04 09:55:32 +0600

    09-Array-splice.js

---
+++ Modern JavaScript ES6/09-Array-splice.js
@@ -0,0 +1,10 @@
+// 09-Array-splice.js
+
+let number = [1, 2, 3, 4, 5, 6];
+
+var result = number.splice(1, 2, 2, 6, 7);
+
+console.log(number);
+console.log(result);
+
+// Changed the main array and return an array
