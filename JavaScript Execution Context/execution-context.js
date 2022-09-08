f6f287972a443a3e958d30b3c2db0c2e6ad7baef
AlifSakib <alifsakib@gmail.com>
2022-09-09 03:46:36 +0600

    Execution Context Done

---
+++ JavaScript Execution Context/execution-context.js
@@ -0,0 +1,20 @@
+/* 
+
+!How JavaScript Code Runs ?
+
+? Amader likha code gula jokhon browser / server pay , tokhon brwoser JS engine (alada alada browser er alada alada engine) er maddhome amader likha code gula diye dey , engine ei code gula compile kore machine code e convert kore tokhon amader computer seta bujhte pare and amra output hisebe dekhte pai.
+
+
+! Global Execution Context 
+
+Phase : Loading / Creation
+window : global Object
+this : window
+Variable Object
+function()
+scope chain
+
+JavaScript code interprete kora shuru korle 1st e global execution context e jay , the jokhon function call kori tokhon function execution context e jay.
+
+! Function Execution Context 
+*/
