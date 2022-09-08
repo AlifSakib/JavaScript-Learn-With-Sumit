c816b5f0e81f767c8156bdd2fde6385984037ee9
AlifSakib <alifsakib@gmail.com>
2022-09-08 10:13:47 +0600

    Closure

---
+++ 77-JavaScript-Function-Closures.js
@@ -0,0 +1,29 @@
+// 77-JavaScript-Function-Closures.js
+
+// !JavaScript Colusers
+
+// !Global variable can be made local(private) with closures
+
+/* 
+
+! Global Variable
+
+function myFunction(){
+    let a = 4
+    return a * a
+}
+
+*/
+
+/* function add() {
+  let counter = 0;
+  counter += 1;
+  return counter;
+}
+
+add();
+add();
+add();
+
+console.log(counter);
+ */
