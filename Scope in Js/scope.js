c55dbab477b77b37aad7456aa74d4b8b3991cde6
AlifSakib <alifsakib@gmail.com>
2022-09-10 02:36:23 +0600

    JavaScript Scope

---
+++ Scope in Js/scope.js
@@ -0,0 +1,14 @@
+const x = "apple";
+
+//Parent er dunya
+
+function fruit() {
+  //child
+  const y = "banana";
+  console.log(`${x} from my Fruits`);
+}
+
+fruit();
+console.log(x);
+
+// Parent child ke shob diye dey but child parent ke kichu dey na .
