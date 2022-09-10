ade31a30300fc9b168b4847e3fdd73fac6eb0532
AlifSakib <alifsakib@gmail.com>
2022-09-10 13:03:55 +0600

    15-JavaScript-String.js

--- JavaScript W3School/15-JavaScript-String.js
+++ JavaScript W3School/15-JavaScript-String.js
@@ -1,9 +1,4 @@
 // TODO: 15-JavaScript-String.js
 
-//dot sign diye jeta call kori oita hocche property
-
 let text = "there are a lot of Programing language without JS";
-
 console.log(text);
-
-//! Object ke kokhono == diye compare kora jay na , mainly duita object ke kokhonoi compare kora jay na , so comparing two object will always return false
