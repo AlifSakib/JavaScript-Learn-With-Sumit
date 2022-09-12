6917b23a5942f75455eddf1beb4e6419f3cc5bc9
AlifSakib <alifsakib@gmail.com>
2022-09-12 11:33:22 +0600

    16-JavaScript-String-Methods.js

--- JavaScript W3School/16-JavaScript-String-Methods.js
+++ JavaScript W3School/16-JavaScript-String-Methods.js
@@ -13,3 +13,26 @@ Length,slice(), substring(), substr(), replace(), toUpperCase(), toLowerCase(), 
 // ? Scape Character
 const str = "this is \\Vaikings\\ Row";
 console.log(str);
+
+// JavaScript String slice() & subStr()
+
+let string = "Apple, Banana, Kiwi";
+
+let part = string.slice(7, 13);
+console.log(part);
+
+// Slice er somoy amra bole dicchi koto theke koto . ar subStr er belat 1st pera meter hobe koto theke kop dibo eropr and erpor jekhane kop dibo oikhan theke koto ghor jabo.
+
+let string2 = "Apple, Banana, Kiwi";
+
+let part2 = string2.substr(7);
+console.log(part2);
+
+// * Replace Method
+
+let text = "Please visit Microsoft!";
+
+let newText = text.replace("Microsoft", "W3School");
+console.log(newText);
+
+// replace method doesnot change the main string , it gives a new string
