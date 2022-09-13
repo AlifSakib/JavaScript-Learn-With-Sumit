712b0d4320d63db7ae056a6dfa5d9bd1aadc2f13
AlifSakib <alifsakib@gmail.com>
2022-09-13 20:30:45 +0600

    Array Sort

--- JavaScript W3School/23-JavaScript-Array-Sort.js
+++ JavaScript W3School/23-JavaScript-Array-Sort.js
@@ -7,14 +7,61 @@
 
 // this is for assending format
 const points = [40, 100, 1, 5, 25, 10];
-points.sort(function(a, b){return a - b});
+points.sort(function (a, b) {
+  return a - b;
+});
+console.log(points);
 
 // array descending:
 const point = [40, 100, 1, 5, 25, 10];
-points.sort(function(a, b){return b - a});
+points.sort(function (a, b) {
+  return b - a;
+});
+console.log(points);
 
+const pointss = [40, 100, 1, 5, 25, 10];
 
+pointss.sort(function (a, b) {
+  return 0.5 - Math.random();
+});
 
+console.log(pointss);
 
+//! Fisher Yates Method
 
+//? Later Watch
 
+//! Math.max.apply()
+
+const pointsss = [40, 100, 1, 5, 25, 10];
+
+console.log(Math.max.apply(null, pointsss));
+console.log(Math.min.apply(null, pointsss));
+
+//! Sorting Object Arrays
+
+const cars = [
+  { type: "Volvo", year: 2016 },
+  { type: "Saab", year: 2001 },
+  { type: "BMW", year: 2010 },
+];
+
+cars.sort(function (a, b) {
+  return a.year - b.year;
+});
+
+console.log(cars);
+
+cars.sort(function (a, b) {
+  x = a.type.toLowerCase();
+  y = b.type.toLowerCase();
+
+  if (x > y) {
+    return 1;
+  } else {
+    return -1;
+  }
+  return 0;
+});
+
+console.log(cars);
