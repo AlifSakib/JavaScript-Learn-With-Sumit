07b97ac4fd01729a4b5ba5c9b8ba5e05c52bdd87
AlifSakib <alifsakib@gmail.com>
2022-09-14 14:15:32 +0600

    32-Booleans.js

---
+++ JavaScript W3School/32-Booleans.js
@@ -0,0 +1,21 @@
+// 32-Booleans.js
+
+/* 
+
+? Everything with a value is true , Everything without a value is false .
+
+
+! Falsy Values : 
+
+* 0
+* -0
+* false
+* null
+* undefined
+* Nan
+* empty String
+
+else everything is true n JavaScript
+
+
+*/
