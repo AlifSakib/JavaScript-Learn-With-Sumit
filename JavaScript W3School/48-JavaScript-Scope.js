4da15adda489638e42a60596980e6f84bba31342
AlifSakib <alifsakib@gmail.com>
2022-09-15 15:04:50 +0600

    48-JavaScript-Scope.js

---
+++ JavaScript W3School/48-JavaScript-Scope.js
@@ -0,0 +1,23 @@
+// 48-JavaScript-Scope.js
+
+//? Js Scope
+
+/* 
+
+* Block Scope
+* Function Scope
+* Global Scope
+
+Let and Const keyword provide Block Scope
+
+*/
+
+//*  Local Scope
+
+// Variables declared within a function , become local to the functio
+
+/* 
+
+function scope er moddhe var , let , const er value baire theke accessable na , so var is a globa plus function scope. but amra jodi if-else block er moddhe var likhi oita baire theke accessable but jodi let / const liki if bock er moddhe then oi let / const baire theke acceptable na .
+
+*/
