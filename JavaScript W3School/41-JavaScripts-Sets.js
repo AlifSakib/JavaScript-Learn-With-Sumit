072c14fd9e3cb3721050389d45051d56d4a6f49a
AlifSakib <alifsakib@gmail.com>
2022-09-15 08:52:42 +0600

    41-JavaScripts-Sets.js

--- JavaScript W3School/41-JavaScripts-Sets.js
+++ JavaScript W3School/41-JavaScripts-Sets.js
@@ -18,3 +18,24 @@
 * size()
 
 */
+
+const arr = [1, 2, 2, 3, 4, 5, 6];
+
+const set = new Set(arr);
+set.add(10);
+set.forEach((item) => {
+  console.log(item);
+});
+
+// we can also add value in a set using variable
+
+const a = 11;
+const b = 15;
+const c = 1;
+
+set.add(a);
+set.add(b);
+set.add(c);
+
+console.log(arr);
+console.log(set);
