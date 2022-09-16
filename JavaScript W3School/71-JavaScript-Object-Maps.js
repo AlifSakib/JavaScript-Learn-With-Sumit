c05bd771eeef53759df76f701b109a13026360f1
AlifSakib <alifsakib@gmail.com>
2022-09-16 21:01:36 +0600

    71-JavaScript-Object-Maps.js

--- JavaScript W3School/71-JavaScript-Object-Maps.js
+++ JavaScript W3School/71-JavaScript-Object-Maps.js
@@ -1,1 +1,15 @@
 // 71-JavaScript-Object-Maps.js
+
+// self invoked function
+
+(function () {
+  console.log("I am A");
+})(); // self invoked function or IIFE -> immediately invoked function, Its also an expression
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+const result = myFunction(3, 4);
+
+console.log(myFunction.toString());
---
+++ JavaScript W3School/72-JavaScript-Function-Definitions.js
@@ -0,0 +1,1 @@
+// 72-JavaScript-Function-Definitions.js
